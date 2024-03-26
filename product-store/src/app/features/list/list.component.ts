65360759de6536ee518648210f91cb35a7bc481b
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-03-26 19:07:01 -0300

    feat: listas de produtos

--- product-store/src/app/features/list/list.component.ts
+++ product-store/src/app/features/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  products: any[] = [];
+  
+  ngOnInit(){
+    this.httpClient.get<any>('/api/products').subscribe((products) => {
+      this.products = products
+    })
+  }
 
 }
