fc1fb6f98493f31f05114d7b84679c3d5257b46e
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-03-27 19:45:34 -0300

    refactor: carrega createComponent via lazy load

--- product-store/src/app/app.routes.ts
+++ product-store/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
-import { CreateComponent } from './features/create/create.component';
 
 export const routes: Routes = [
     {
@@ -9,6 +8,7 @@ export const routes: Routes = [
     },
     {
         path: 'create-product',
-        component: CreateComponent
+        loadComponent: () =>
+        import('./features/create/create.component').then((m) => m.CreateComponent),
     }
 ];
