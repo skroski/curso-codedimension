777621509bca4b7a75bc778f463eb173f3c9ccf4
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-03-27 02:33:58 -0300

    feat: adiciona botal criar produto

--- product-store/src/app/features/list/list.component.ts
+++ product-store/src/app/features/list/list.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   template: `
+  <div class="action-container">
+    <a mat-raised-button color="primary" [routerLink]="['create-product']">Criar Produto</a>
+  </div>
   @for (product of products; track product.id) {
     <app-card [product]="product"></app-card>
   }
