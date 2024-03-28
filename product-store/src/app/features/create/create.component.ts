752c332336cb2368fcbbfe522290feb120bc8161
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-03-28 15:12:45 -0300

    refactor: onSubmit method

--- product-store/src/app/features/create/create.component.ts
+++ product-store/src/app/features/create/create.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
-  templateUrl: './create.component.html',
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  template: `
+  <form [formGroup]="form" (ngSubmit)="onSubmit">
+  <p>
+  <mat-form-field>
+    <mat-label>Titulo Produto</mat-label>
+    <input matInput type="text" formControlName="title" placeholder="Título">
+
+  </mat-form-field>
+
+</p>
+<p>
+  <button type="submit">Salvar Produto</button>
+</p>
+</form>
+  `,
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  form = new FormGroup({
+
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
+
+  onSubmit() {
+
+    this.form.controls.title.value;
+
+  }
 
 }
