070e42c6ca6e1ab870548a5dec5c21cad5e69bc8
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-04-02 08:18:04 -0300

    feat: adicionado campos de form create

--- product-store/src/app/features/create/create.component.ts
+++ product-store/src/app/features/create/create.component.ts
@@ -1,26 +1,23 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   template: `
-  <form [formGroup]="form" (ngSubmit)="onSubmit">
-  <p>
-  <mat-form-field>
-    <mat-label>Titulo Produto</mat-label>
-    <input matInput type="text" formControlName="title" placeholder="Título">
-
-  </mat-form-field>
-
-</p>
-<p>
-  <button type="submit">Salvar Produto</button>
-</p>
-</form>
+      <form [formGroup]="form" (ngSubmit)="onSubmit" class="form-container">
+        <div class="form-content-container">
+          <mat-form-field>
+            <mat-label>Titulo Produto</mat-label>
+            <input matInput type="text" formControlName="title" placeholder="Título">
+          </mat-form-field>
+          <button type="submit" mat-raised-button color="accent">Salvar Produto</button>
+        </div>
+      </form>
   `,
   styleUrl: './create.component.scss'
 })
