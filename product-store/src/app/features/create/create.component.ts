c657e44a6f3ce3c90a56ba0ff09ad55a9c192207
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-05-04 18:04:02 -0300

    feat: botão back para a home

--- product-store/src/app/features/create/create.component.ts
+++ product-store/src/app/features/create/create.component.ts
@@ -4,14 +4,17 @@ import { MatSnackBar } from '@angular/material/snack-bar'
 import { Router } from '@angular/router';
 import { Product } from '../../shared/interfaces/product.interface';
 import { FormComponent } from "../../shared/components/form/form.component";
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 @Component({
     selector: 'app-create',
     standalone: true,
     template: `
+    <app-back-to-list></app-back-to-list>
     <app-form (done)="onSubmit($event)"></app-form>
   `,
     styleUrl: './create.component.scss',
-    imports: [FormComponent]
+    imports: [FormComponent, BackToListComponent
+    ]
 })
 export class CreateComponent {
   productService = inject(ProductsService);
--- product-store/src/app/features/edit/edit.component.ts
+++ product-store/src/app/features/edit/edit.component.ts
@@ -4,13 +4,16 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { FormComponent } from '../../shared/components/form/form.component';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   template: `
+  <app-back-to-list></app-back-to-list>
 <app-form [product]="product" (done)="onSubmit($event)"></app-form>
+
 `,
   styleUrl: './edit.component.scss'
 })
